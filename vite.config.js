441b5aa5710a5387d0746bb03362b33eb0756006
DaikonDEV <joel.cruz.b03@gmail.com>
2024-09-07 09:28:32 -0600

    new chages 2

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://joelcb23.github.io/",
+  base: "https://joelcb23.github.io/react-travels-site/",
 });
